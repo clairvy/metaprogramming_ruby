d814a3244fbc5440d83c415ee461e62746678c49
NAGAYA Shinichiro <clairvy@gmail.com>
2011-02-17 01:50:55 +0900

    Ruby method_missing でリファクタリング

--- ruby/lib/methods/computer/boring.rb
+++ ruby/lib/methods/computer/boring.rb
@@ -2,16 +2,14 @@ class Computer
   def initialize(computer_id, data_source)
     @id = computer_id
     @data_source = data_source
-    data_source.methods.grep(/^get_(.*)_info$/) { Computer.define_component $1 }
   end
 
-  def self.define_component(name)
-    define_method(name) {
-      info = @data_source.send("get_#{name}_info", @id)
-      price = @data_source.send("get_#{name}_price", @id)
-      result = "#{name.capitalize}: #{info} (#{price})"
-      return "* #{result}" if price > 100
-      result
-    }
+  def method_missing(name, *args)
+    super if !@data_source.respond_to?("get_#{name}_info")
+    info = @data_source.send("get_#{name}_info", @id)
+    price = @data_source.send("get_#{name}_price", @id)
+    result = "#{name.to_s.capitalize}: #{info} (#{price})"
+    return "* #{result}" if price > 100
+    result
   end
 end
