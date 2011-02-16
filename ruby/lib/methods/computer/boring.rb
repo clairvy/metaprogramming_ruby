5619bf97586c40a79a1ff87121b47d99e31f0896
NAGAYA Shinichiro <clairvy@gmail.com>
2011-02-17 01:38:40 +0900

    Ruby 動的メソッドでリファクタリング

--- ruby/lib/methods/computer/boring.rb
+++ ruby/lib/methods/computer/boring.rb
@@ -2,29 +2,16 @@ class Computer
   def initialize(computer_id, data_source)
     @id = computer_id
     @data_source = data_source
+    data_source.methods.grep(/^get_(.*)_info$/) { Computer.define_component $1 }
   end
 
-  def mouse
-    info = @data_source.get_mouse_info(@id)
-    price = @data_source.get_mouse_price(@id)
-    result = "Mouse: #{info} (#{price})"
-    return "* #{result}" if price > 100
-    result
-  end
-
-  def cpu
-    info = @data_source.get_cpu_info(@id)
-    price = @data_source.get_cpu_price(@id)
-    result = "Cpu: #{info} (#{price})"
-    return "* #{result}" if price > 100
-    result
-  end
-
-  def keyboard
-    info = @data_source.get_keyboard_info(@id)
-    price = @data_source.get_keyboard_price(@id)
-    result = "Keyboard: #{info} (#{price})"
-    return "* #{result}" if price > 100
-    result
+  def self.define_component(name)
+    define_method(name) {
+      info = @data_source.send("get_#{name}_info", @id)
+      price = @data_source.send("get_#{name}_price", @id)
+      result = "#{name.capitalize}: #{info} (#{price})"
+      return "* #{result}" if price > 100
+      result
+    }
   end
 end
