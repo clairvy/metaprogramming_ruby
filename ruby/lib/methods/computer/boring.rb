e27d222115a5c41b6f9b911d87317c05b6f2725d
NAGAYA Shinichiro <clairvy@gmail.com>
2011-02-17 02:08:57 +0900

    Ruby method_missing を更につっこんで

--- ruby/lib/methods/computer/boring.rb
+++ ruby/lib/methods/computer/boring.rb
@@ -1,15 +1,23 @@
 class Computer
+  instance_methods.each do |m|
+    undef_method m unless m.to_s =~ /^__|object_id|method_missing|respond_to?/
+  end
+
   def initialize(computer_id, data_source)
     @id = computer_id
     @data_source = data_source
   end
 
   def method_missing(name, *args)
-    super if !@data_source.respond_to?("get_#{name}_info")
+    super if !respond_to?(name)
     info = @data_source.send("get_#{name}_info", @id)
     price = @data_source.send("get_#{name}_price", @id)
     result = "#{name.to_s.capitalize}: #{info} (#{price})"
     return "* #{result}" if price > 100
     result
+  end
+
+  def respond_to?(method)
+    @data_source.respond_to?("get_#{method}_info") || super
   end
 end
