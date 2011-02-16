dfffa0d08a0baf5baa7992a0013d5983459bd94a
NAGAYA Shinichiro <clairvy@gmail.com>
2011-02-17 02:05:47 +0900

    Ruby method_missing の罠のコードを追加

---
+++ ruby/sample/methods/bug_hunt.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby19
+
+class Roulette
+  def method_missing(name, *args)
+    person = name.to_s.capitalize
+    # solution
+    super unless %w[Bob Frank Bill].include? person
+    number = 0
+    # solution
+    3.times do
+      number = rand(10) + 1
+      puts "#{number}..."
+    end
+    "#{person} got a #{number}"
+  end
+end
+
+number_of = Roulette.new
+puts number_of.bob
+puts number_of.frank
+
